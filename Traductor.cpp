b9dce22c3c27a7e7068aa9c43c23d7af9bb62cc4
unknown <ricardoarrecisrivera@gmail.com>
2025-04-27 22:45:05 -0600

    Se crea archivo inicial del programa

---
+++ Traductor.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// Estructura para almacenar cada palabra
+struct Palabra {
+    string traduccion;
+    string funcionalidad;
+};
+
+// Mapa global de palabras
+map<string, Palabra> diccionario;
+
+// Ruta del archivo donde se guardan las palabras
+const string archivoDiccionario = "diccionario.txt";
+
+void limpiarConsola() {
+    system("cls"); 
+}
+
+// Función para cargar palabras del archivo
+void cargarDiccionario() {
+    diccionario.clear();
+	ifstream archivo(archivoDiccionario.c_str());
+    if (!archivo.is_open()) return;
+
+    string linea;
+    while (getline(archivo, linea)) {
+        stringstream ss(linea);
+        string palabra, traduccion, funcionalidad;
+
+        getline(ss, palabra, '|');
+        getline(ss, traduccion, '|');
+        getline(ss, funcionalidad, '|');
+
+        diccionario[palabra] = {traduccion, funcionalidad};
+    }
+    archivo.close();
+}
+
+// Función para guardar palabras al archivo
+void guardarDiccionario() {
+    ofstream archivo(archivoDiccionario.c_str(), ios::trunc);  // Truncar el archivo antes de escribir
+    if (!archivo.is_open()) {
+        cout << "Error al abrir el archivo para guardar.\n";
+        return;
+    }
+    
+    for (map<string, Palabra>::iterator it = diccionario.begin(); it != diccionario.end(); ++it) {
+        archivo << it->first << "|" << it->second.traduccion << "|" << it->second.funcionalidad << "\n";
+    }
+    archivo.close();
+}
